4ea979172dc92204ee54d659ed0d5765179bab00
Michael Yan <rain@rainboyan.com>
2026-08-28 03:43:51 +0800

    Release v0.1.0

--- plugin/src/main/groovy/org/graceframework/plugins/jsbundling/JsBundlingGrailsPlugin.groovy
+++ plugin/src/main/groovy/org/graceframework/plugins/jsbundling/JsBundlingGrailsPlugin.groovy
@@ -19,7 +19,7 @@ import grails.plugins.Plugin
 
 class JsBundlingGrailsPlugin extends Plugin {
 
-    def version = '0.1.0-SNAPSHOT'
+    def version = '0.1.0'
     def grailsVersion = "2023.0.0 > *"
     def loadAfter = ['core']
 
